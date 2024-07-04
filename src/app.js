f4ac78a5308ad60db8edcce93d06f3a408bb3265
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-07-04 13:57:44 +0000

    Added new card button

--- src/app.js
+++ src/app.js
@@ -5,27 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const numbers = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-
-const suits = ["♦", "♥", "♠", "♣"];
+const newCard = document.getElementById("new-card");
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
   generateRandomNumber();
   generateRandomSuit();
 };
@@ -35,25 +18,52 @@ function randomNumber(minNum, maxNum) {
 }
 
 function generateRandomNumber() {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
   const numberP = document.querySelector(".number");
 
-  const randomIndex = randomNumber(0, numbers.length - 1);
+  const randomIndex = randomNumber(0, numbers.length);
 
   numberP.textContent = numbers[randomIndex];
 }
 
 function generateRandomSuit() {
+  const suits = ["♦", "♥", "♠", "♣"];
   const topSuit = document.querySelector(".top-suit");
   const bottomSuit = document.querySelector(".bottom-suit");
 
-  const randomIndex = randomNumber(0, suits.length - 1);
+  // Generate random suit based on index of suits array
+  const randomIndex = randomNumber(0, suits.length);
   const randomSuit = suits[randomIndex];
 
+  // If suit is heart or diamong change color of suit to red
   if (randomSuit === "♥" || randomSuit === "♦") {
     topSuit.style.color = "red";
     bottomSuit.style.color = "red";
+  } else {
+    topSuit.style.color = "black";
+    bottomSuit.style.color = "black";
   }
 
   topSuit.textContent = randomSuit;
   bottomSuit.textContent = randomSuit;
 }
+
+newCard.addEventListener("click", () => {
+  generateRandomNumber();
+  generateRandomSuit();
+});
